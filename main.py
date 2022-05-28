741188ea337f19eeedb7a11b55bcfbd247ddaaf1
Oluwadara <oluwadarafabunmi@yahoo.com>
2022-05-29 00:29:22 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -6,11 +6,15 @@
 
 def find_anagram(word, anagram):
     # [assignment] Add your code here
+    word = input('Enter word here: ')
+    print(word)
+    anagram = input('Enter anagram here: ')
+    print(anagram)
     if(sorted(word) == sorted(anagram)):
         return True
     else:
         return False
 
-find_anagram('dara', 'arad')
+find_anagram('word', 'anagram')
     
 
